02ba58083cc5c8a5c2f6a03d8df100d0e4b7d609
rics9t <andreygamer190@gmail.com>
2026-02-12 21:43:19 +0500

    Update UnknownResource.java

--- common/src/main/java/foundry/veil/api/resource/type/UnknownResource.java
+++ common/src/main/java/foundry/veil/api/resource/type/UnknownResource.java
@@ -14,21 +14,3 @@ public record UnknownResource(VeilResourceInfo resourceInfo) implements VeilReso
     public void hotReload() {
     }
 }
---- START OF FILE common/src/main/java/foundry/veil/api/resource/type/McMetaResource.java ---
-package foundry.veil.api.resource.type;
-
-import foundry.veil.api.resource.VeilResource;
-import foundry.veil.api.resource.VeilResourceInfo;
-import net.minecraft.server.packs.resources.ResourceMetadata;
-
-public record McMetaResource(VeilResourceInfo resourceInfo, ResourceMetadata metadata) implements VeilResource<McMetaResource> {
-
-    @Override
-    public boolean canHotReload() {
-        return false;
-    }
-
-    @Override
-    public void hotReload() {
-    }
-}
