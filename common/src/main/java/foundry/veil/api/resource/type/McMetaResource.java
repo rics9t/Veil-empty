6338fba9ec504ed65b7d1f9550d5a9d41f978b9a
rics9t <andreygamer190@gmail.com>
2026-02-12 21:43:40 +0500

    Clean up McMetaResource by removing unused code
    Removed unused methods and imports from McMetaResource.

--- common/src/main/java/foundry/veil/api/resource/type/McMetaResource.java
+++ common/src/main/java/foundry/veil/api/resource/type/McMetaResource.java
@@ -1,18 +1,10 @@
 package foundry.veil.api.resource.type;
 
 import foundry.veil.api.resource.VeilResource;
-import foundry.veil.api.resource.VeilResourceAction;
 import foundry.veil.api.resource.VeilResourceInfo;
 import net.minecraft.server.packs.resources.ResourceMetadata;
 
-import java.util.List;
-
 public record McMetaResource(VeilResourceInfo resourceInfo, ResourceMetadata metadata) implements VeilResource<McMetaResource> {
-
-    @Override
-    public List<VeilResourceAction<McMetaResource>> getActions() {
-        return List.of();
-    }
 
     @Override
     public boolean canHotReload() {
@@ -21,10 +13,5 @@ public record McMetaResource(VeilResourceInfo resourceInfo, ResourceMetadata met
 
     @Override
     public void hotReload() {
-    }
-
-    @Override
-    public int getIconCode() {
-        return 0xECEA; // Info file icon
     }
 }
